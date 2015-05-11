fadced425ba77efc04d1a94ebdeadf5f870466fb
macallhanbo <416939491@qq.com>
2015-05-11 18:07:20 +0800

    main page title

--- Apps/AppSrc/Home/Controller/IndexController.class.php
+++ Apps/AppSrc/Home/Controller/IndexController.class.php
@@ -6,6 +6,7 @@ class IndexController extends HomeBaseController {
 
     public function indexAction() {
 //        echo 'this is welcome page!';
+        $this->assign('title_name', '泰游记，一个专为泰国旅游服务的网站');
         $this->display();
     }
 
