56378c1fd79de1ef59529691acb06d4a928749a6
macallhanbo <416939491@qq.com>
2015-06-12 16:48:10 +0800

    captcha

--- Apps/AppSrc/Home/Controller/AjaxController.class.php
+++ Apps/AppSrc/Home/Controller/AjaxController.class.php
@@ -3,6 +3,7 @@
 namespace Home\Controller;
 
 use Home\Logic\UserLogic;
+use Home\Service\ThirdParty\CaptchaService;
 
 class AjaxController extends HomeBaseController {
 
@@ -11,7 +12,8 @@ class AjaxController extends HomeBaseController {
      * created_at:2015-5-15 13:08
      * updated_at:2015-5-15 17:35
      */
-    public function registerEmailValidationAction() {
+    public function registerEmailValidationAction() 
+    {
         $email = I('post.email');
         $data = array();
         $data['email_resp'] = '';
@@ -19,7 +21,8 @@ class AjaxController extends HomeBaseController {
         
         //邮箱格式不正确
         $is_email = eregi(EMAIL_VALIDATION, $email);
-        if(!$is_email){
+        if(!$is_email)
+        {
             $data['email_resp'] = C('email_invalid');
             $data['email_registerable'] = NO;
             $this->json_success_response($data);
@@ -30,7 +33,8 @@ class AjaxController extends HomeBaseController {
         $user = new UserLogic();
         $is_registered = $user->validate_user_email($email);
         
-        if($is_registered){
+        if($is_registered)
+        {
             $data['email_resp'] = C('email_registered');
             $data['email_registerable'] = NO;
             $this->json_success_response($data);
@@ -38,5 +42,11 @@ class AjaxController extends HomeBaseController {
         }
         //邮箱可以注册
         $this->json_success_response($data);
+    }
+    
+    
+    public function createCaptchaImageAction()
+    {
+        $captcha = new CaptchaService();
     }
 }
